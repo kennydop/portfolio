dff4c2c6bfd3fe12463544c22c16ff9963dfd2ad
Patrick Danso Offei <dansooffeipatrick@gmail.com>
2022-04-06 11:02:28 +0000

    slider and mail sender

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 import "./style.css";
 
 const parallaxImgs = document.querySelectorAll(".pp");
-const hamToggleButton = document.querySelectorAll(".ham-checkbtn")[0];
-const navbarLinks = document.querySelectorAll(".nav")[0];
+const hamToggleButton = document.querySelector(".ham-checkbtn");
+const navbarsm = document.querySelector(".nav");
+const navbarLinks = document.querySelectorAll(".navbarLink");
+const moveLeftButton = document.querySelector(".mbleft");
+const moveRightButton = document.querySelector(".mbright");
+const projectsView = document.querySelector(".pview");
+const subjectField = document.querySelector("#subject");
+const messageField = document.querySelector("#message");
+const sendMailBtn = document.querySelector("#form-submit");
+const formFields = [subjectField, messageField];
+var mail = {
+  subject: "",
+  message: "",
+};
 
 const parallaxSocials = (e) => {
   parallaxImgs.forEach((parallaxImg) => {
@@ -16,5 +28,39 @@ const parallaxSocials = (e) => {
 };
 document.addEventListener("mousemove", parallaxSocials);
 hamToggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+  navbarsm.classList.toggle("active");
+});
+
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelectorAll(".navbarLink").forEach((link) => {
+      if (link.classList.contains("active")) {
+        link.classList.remove("active");
+      }
+    });
+    link.classList.toggle("active");
+    navbarsm.classList.remove("active");
+  });
+});
+
+moveLeftButton.addEventListener("click", () => {
+  projectsView.scrollLeft >= 0
+    ? (projectsView.scrollLeft -= document.documentElement.clientWidth)
+    : (projectsView.scrollLeft = projectsView.scrollLeft);
+});
+moveRightButton.addEventListener("click", () => {
+  projectsView.scrollLeft <
+  projectsView.scrollWidth - projectsView.offsetWidth - 250
+    ? (projectsView.scrollLeft += document.documentElement.clientWidth)
+    : (projectsView.scrollLeft = 0);
+});
+
+formFields.forEach((field) => {
+  field.addEventListener("keyup", (e) => {
+    mail.subject = document.querySelector("#subject").value;
+    mail.message = document.querySelector("#message").value;
+    console.log(mail);
+    var mailLink = `mailto:dansooffeipatrick@gmail.com?subject=${mail.subject}&body=${mail.message}`;
+    sendMailBtn.href = mailLink;
+  });
 });
